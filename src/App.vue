b90658c72c63338ec5313e262facc8a3153eb2a3
alrivasp <alvaro.rivas.p@gmail.com>
2021-09-26 22:09:15 -0300

    Desafio Terminado

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Crear una nueva tarea</h1>
+    Tarea <input id="datos" v-model="tarea">
+    <button v-on:click="agregar">Crear</button>
+    <br>
+    <h3>Lista</h3>
+    <ul v-for="(task, index) in tareas" :key="index">
+        <li>{{index}}.- {{task}}</li>
+    </ul>
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return { tarea: "", tareas: [] }
+  },
+  methods: {
+      agregar: function(){
+          this.tareas.push(this.tarea);
+      }
+  }
+};
+</script>
